6a745916cf00ee42d50c06171f3ee1dae364ee6d
plyn85 <plyn99@gmail.com>
2020-02-17 16:40:50 +0000

    changing login an register html, an refactoring css

--- app.py
+++ app.py
@@ -3,38 +3,38 @@ from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 import os
 from os import path
-if path.exists("env.py"):
+if path.exists('env.py'):
     import env
 
 
 app = Flask(__name__)
 
-app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
-app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
-app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+app.config['MONGO_DBNAME'] = os.environ.get('MONGO_DBNAME')
+app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
 
-@app.route("/")
+@app.route('/')
 def base():
     return render_template('base.html')
 
 
-@app.route("/index")
+@app.route('/index')
 def index():
     return render_template('index.html')
 
 
-@app.route("/register")
+@app.route('/register')
 def register():
     return render_template('register.html')
 
 
-@app.route("/login")
+@app.route('/login')
 def login():
     return render_template('login.html')
 
 
 if __name__ == '__main__':
-    app.run(host=os.environ.get("IP"),
-            port=int(os.environ.get("PORT")),
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
             debug=False)
