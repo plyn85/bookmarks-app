fa196d7b1c9e43f89aab7f0dbd26feea53f74524
plyn85 <plyn99@gmail.com>
2020-03-14 16:15:12 +0000

    adding pages to direct a user that has yet to add a category or bookmark

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ from os import path
 if path.exists('env.py'):
     import env
 
-
 app = Flask(__name__)
 #  configuring enviroment variables which are set in env.py
 app.config['MONGO_DBNAME'] = os.environ.get('MONGO_DBNAME')
@@ -21,13 +20,14 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
 mongo = PyMongo(app)
 moment = Moment(app)
+
 # index,  login, register, and log out section -----------------------------------------
 @app.route('/index')
 @app.route('/')
 def index():
-    users = mongo.db.users.find()
     categories = mongo.db.categories.find()
     bookmarks = mongo.db.bookmarks.find()
+    users = mongo.db.users.find()
     return render_template('index.html', categories=categories, bookmarks=bookmarks, users=users)
 
 
@@ -48,7 +48,7 @@ def login():
                 flash(f'You are logged in!', 'success')
                 return redirect(url_for('index'))
         flash(
-            f'Login  Unsuccessful. Please check username/password conbination', 'danger')
+            f'Login  Unsuccessful. Please check username/password combination', 'danger')
 
     return render_template('login.html')
 
@@ -82,12 +82,22 @@ def logout():
 
 # user section
 @app.route('/users')
+# if a user has not yet added a bookmark the newuser page will be render
+# and if the user has bookmarks already added the user page will render
 def users():
     users = mongo.db.users.find()
     categories = mongo.db.categories.find()
     bookmarks = mongo.db.bookmarks.find()
+    book_name = mongo.db.bookmarks.find_one(
+        {'username': session.get('username')})
+    if book_name is None:
+        return render_template('newuser.html')
+
     return render_template('users.html', users=users, bookmarks=bookmarks, categories=categories)
-# bookmarks section ---------------------------------------------------------------------
+
+    # bookmarks section ---------------------------------------------------------------------
+
+
 @app.route('/add_bookmark')
 def add_bookmark():
     categories = mongo.db.categories.find()
@@ -109,10 +119,10 @@ def insert_bookmark():
 
 @app.route("/edit_bookmark/<book_id>")
 def edit_bookmark(book_id):
-    users = mongo.db.users.find()
+    user = mongo.db.users.find()
     all_categories = mongo.db.categories.find()
     the_bookmark = mongo.db.bookmarks.find_one({"_id": ObjectId(book_id)})
-    return render_template("edit_bookmark.html", book=the_bookmark, categories=all_categories, users=users)
+    return render_template("edit_bookmark.html", book=the_bookmark, categories=all_categories, user=user)
 
 
 @app.route('/update_bookmark/<book_id>', methods=["POST"])
@@ -148,8 +158,12 @@ def remove_bookmark(book_id):
 def user_categories():
     categories = mongo.db.categories.find()
     bookmarks = mongo.db.bookmarks.find()
-    users = mongo.db.users.find()
-    return render_template('categories.html', categories=categories, bookmarks=bookmarks, users=users)
+    user = mongo.db.users.find()
+    cat_name = mongo.db.categories.find_one(
+        {'username': session.get('username')})
+    if cat_name is None:
+        return render_template('newuser_cat.html')
+    return render_template('categories.html', categories=categories, bookmarks=bookmarks, user=user)
 
 
 @app.route('/add_category')
@@ -159,7 +173,7 @@ def add_category():
 
 @app.route('/insert_category', methods=["POST"])
 def insert_category():
-    flash(f'Your category has been added!', 'success')
+    flash(f'Your category has been added! It will be now be available in the add bookmarks section In the dropdown menu', 'success')
     category = mongo.db.categories
     category.insert_one({
         'username': session['username'],
