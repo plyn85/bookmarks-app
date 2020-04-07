09e25068c0010148697bcf43af841b2da959b6fe
plyn85 <plyn99@gmail.com>
2020-04-07 14:03:02 +0100

    add-flashed messages-timeou-function-dissapear-10 secs

--- static/js/main.js
+++ static/js/main.js
@@ -15,3 +15,10 @@
 //     });
 //   }
 // });
+$(document).ready(function () {
+  /* added so flashed messages dissappear after ten seconds */
+
+  setTimeout(function () {
+    $("#flashed-message").hide("fast");
+  }, 10000);
+});
