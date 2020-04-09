bd23850d23a7725bb5d6a5cd71cb942385e9022f
plyn85 <plyn99@gmail.com>
2020-04-09 09:20:17 +0100

    Info  btn stop working check changes since last commit

--- static/js/main.js
+++ static/js/main.js
@@ -1,17 +1,17 @@
-let likeBtnForm = document.getElementsByClassName("like-btn-from");
+// let likeBtnForm = document.getElementsByClassName("like-btn-from");
 
-for (let i = 0; i < likeBtnForm.length; i++) {
-  likeBtnForm[i].addEventListener("submit", function (event) {
-    event.preventDefault();
-  });
-}
+// for (let i = 0; i < likeBtnForm.length; i++) {
+//   likeBtnForm[i].addEventListener("submit", function (event) {
+//     event.preventDefault();
+//   });
+// }
 
 $(document).ready(function () {
   /* added so flashed messages dissappear after ten seconds */
   setTimeout(function () {
     $("#flashed-message").hide("slow");
   }, 10000);
-  // this will hide an show the Instructions text on users category an bookmarks page
+  // this will hide an show the Instructions text on users category an bookmarks
   $(".info-btn").click(function () {
     $(".user-text").toggle();
   });
