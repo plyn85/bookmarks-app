c3bdf31532b7397f1cb97765bf348b5d1ab32215
plyn85 <plyn99@gmail.com>
2020-04-09 13:24:08 +0100

    adding image an member testimonals-index page

--- static/js/main.js
+++ static/js/main.js
@@ -1,16 +1,15 @@
-// let likeBtnForm = document.getElementsByClassName("like-btn-from");
-
-// for (let i = 0; i < likeBtnForm.length; i++) {
-//   likeBtnForm[i].addEventListener("submit", function (event) {
-//     event.preventDefault();
-//   });
-// }
-
 $(document).ready(function () {
   /* added so flashed messages dissappear after ten seconds */
   setTimeout(function () {
     $("#flashed-message").hide("slow");
   }, 10000);
+
+  // overriding default on like the like button here
+  $(".like-btn-form").click(function (event) {
+    event.preventDefault();
+    // run something here to activate function
+  });
+
   // this will hide an show the Instructions text on users category an bookmarks
   $(".info-btn").click(function () {
     $(".user-text").toggle();
