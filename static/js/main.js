67497061a590bdcc6fa3ec1f9162800f76ac72a4
plyn85 <plyn99@gmail.com>
2020-04-09 15:33:31 +0100

    removing prevent default on like button

--- static/js/main.js
+++ static/js/main.js
@@ -5,13 +5,14 @@ $(document).ready(function () {
   }, 10000);
 
   // overriding default on like the like button here
-  $(".like-btn-form").click(function (event) {
-    event.preventDefault();
-    // run something here to activate function
-  });
+  $(".like-btn-form").click(function (event) {});
 
   // this will hide an show the Instructions text on users category an bookmarks
   $(".info-btn").click(function () {
     $(".user-text").toggle();
   });
 });
+
+// p_limit = int(request.args.get('limit', 6))
+// p_offset = int(request.args.get('offset', 0))
+//   "current_url": f"/index?limit={str(p_limit)}&offset={str(p_offset = p_limit)}"
