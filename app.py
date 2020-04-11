f8182a84d0d152bec8922c9aee809a62db1dbab4
plyn85 <plyn99@gmail.com>
2020-04-11 18:58:00 +0100

    formating main.js,main.css and app.py file

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# Imports
+Aqui pegar tu python fulero# Imports
 import os
 from flask import Flask, render_template, redirect, request, url_for, session, flash
 from flask_pymongo import PyMongo, pymongo
@@ -469,3 +469,6 @@ if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
             debug=True)
+
+			
+			
--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,10 @@
-$(document).ready(function () {
+$(document).ready(function() {
   /* added so flashed messages dissappear after ten seconds */
-  setTimeout(function () {
-    $("#flashed-message").hide("slow");
+  setTimeout(function() {
+      $("#flashed-message").hide("slow");
   }, 10000);
   // this will hide an show the Instructions text on users category an bookmarks
-  $(".info-btn").click(function () {
-    $(".user-text").toggle();
+  $(".info-btn").click(function() {
+      $(".user-text").toggle();
   });
 });
